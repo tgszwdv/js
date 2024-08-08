3492a557c0a1225ab9caca7fe7ce0d455f086d53
tgszwdv <102832569+tgszwdv@users.noreply.github.com>
2024-08-07 21:38:07 -0400

    Add files via upload

--- api/scraping.js
+++ api/scraping.js
@@ -1,1 +1,50 @@
+// scraping.js
+const puppeteer = require('puppeteer');
+const axios = require('axios');
 
+async function scrape() {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  
+  const url = 'https://selecao-login.app.ufgd.edu.br/';
+  await page.goto(url, { waitUntil: 'networkidle2' });
+
+  const processos = await page.evaluate(() => {
+    const processos = [];
+    const rows = document.querySelectorAll('tr[ng-repeat="processo in ctrl.inscricoesAbertas track by $index"]');
+    
+    rows.forEach((row, index) => {
+      const cells = row.querySelectorAll('td');
+      const titulo = cells[0].innerText.trim();
+      const descricao = cells[1].innerText.trim().replace('Mostrar mais', '').trim();
+      const periodo = cells[2].innerText.trim();
+      const editalUrl = cells[3].querySelector('a').href;
+      const paginaUrl = cells[4].querySelector('a').href;
+
+      if (!titulo.startsWith('PSIE')) {
+        processos.push({
+          titulo: titulo,
+          descricao: descricao,
+          periodo: periodo,
+          url: paginaUrl,
+          edital: editalUrl
+        });
+      }
+    });
+
+    return processos;
+  });
+
+  await browser.close();
+
+  console.log('Dados extraídos do scraping:', JSON.stringify({ processos: processos }, null, 2));
+
+  try {
+    await axios.put('https://jsonserver-khaki.vercel.app/processosAbertos/1', { processos: processos });
+    console.log('Dados atualizados com sucesso!');
+  } catch (error) {
+    console.error('Erro ao atualizar os dados na API:', error);
+  }
+}
+
+module.exports = scrape;
---
+++ api/server.js
@@ -0,0 +1,35 @@
+const jsonServer = require('json-server');
+const path = require('path');
+const express = require('express');
+const scrape = require('./scraping'); // Importar a função de scraping
+
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+
+server.use('/pages', express.static(path.join(__dirname, '../pages')));
+
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}));
+
+// Adicionar a rota para acionar o scraping
+server.get('/scrape', async (req, res) => {
+    try {
+        await scrape(); // Chama a função de scraping
+        res.send('Scraping concluído com sucesso!');
+    } catch (error) {
+        res.status(500).send('Erro ao executar o scraping.');
+    }
+});
+
+server.use(router);
+
+server.listen(3000, () => {
+    console.log('JSON Server is running');
+});
+
+module.exports = server;
